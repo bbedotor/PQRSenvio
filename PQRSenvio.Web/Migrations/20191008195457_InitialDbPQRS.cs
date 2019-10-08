0c1ace2d31fee667dcb7a41d4a51d50ad55cbc87
SANTIAGO JIMENEZ SUAREZ <santiagojimenez196103@correo.itm.edu.co>
2019-10-08 15:35:24 -0500

    DataBase

---
+++ PQRSenvio.Web/Migrations/20191008195457_InitialDbPQRS.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PQRSenvio.Web.Migrations
+{
+    public partial class InitialDbPQRS : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
